8532bdd6051b8d17a08c9ef65c19bd7ce2c73c96
Jas7277 <jasonsworld21@yahoo.com>
2021-05-09 16:43:13 -0400

    Added the simple shopping list

--- src/Site.java
+++ src/Site.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 
 import java.awt.GridLayout;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class Site {
 	static JFrame frame;
 	static ArrayList<JButton> buttons = new ArrayList<>();
 	static JButton button;
+	static ArrayList<Item> shoppingList = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		frame = new JFrame("Shopping");
@@ -16,16 +18,40 @@ public class Site {
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new GridLayout(1, 1));
 		
-		Item[] item = new Item[3];
-		item[0] = new Chilled("Milk", 0, 1, 1);
-		item[1] = new Chilled("Eggs", 0, 1, 2);
-		item[2] = new Chilled("Creamer", 0, 1, 3);
+		Item[] items = new Item[3];
+		items[0] = new Chilled("Milk", 0, 1, 1);
+		items[1] = new Chilled("Eggs", 0, 4, 21);
+		items[2] = new Chilled("Creamer", 0, 6, 14);
 		
-		for (int i = 0; i < item.length; i++) {
-			button = new JButton(item[i].name);
-			button.setSize(100, 100);
+		for (int i = 0; i < items.length; i++) {
+			button = new JButton(items[i].name);
+			button.setSize(50, 50);
+			
 			buttons.add(button);
 			frame.add(button);
+		}
+		
+		JButton done = new JButton("Done");
+		done.setSize(50, 50);
+		
+		frame.add(done);
+		
+		checkButtonClick(items);
+	}
+	
+	static void checkButtonClick(Item[] items) {
+		for (JButton button: buttons) {
+			button.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					for (Item item: items) {
+						if (item.name == button.getText()) {
+							shoppingList.add(item);
+						}
+					}
+				}
+			});
 		}
 	}
 
