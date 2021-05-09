8247f8b45e828acaf72992b1c8aafb82461068ed
Jas7277 <jasonsworld21@yahoo.com>
2021-05-08 20:53:18 -0400

    Added some more classes, started creating the layout

--- src/Site.java
+++ src/Site.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Site {
 	static JFrame frame;
+	static ArrayList<JButton> buttons = new ArrayList<>();
+	static JButton button;
 	
 	public static void main(String[] args) {
 		frame = new JFrame("Shopping");
@@ -9,6 +12,53 @@ public class Site {
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
+		
+		Food[] food = new Food[2];
+		food[0] = new Milk(0, 1, 1);
+		food[1] = new Eggs(0, 1, 2);
+		
+		for (int i = 0; i < food.length; i++) {
+			button = new JButton(food[i].name);
+			button.setSize(100, 100);
+			buttons.add(button);
+			frame.add(button);
+		}
 	}
 
+}
+
+class Food {
+	String name;
+}
+
+class Milk extends Food {
+	
+	// Location variables
+	int aisle;
+	int bin;
+	int pos;
+	
+	String name = "Milk";
+	
+	Milk(int aisle, int bin, int pos) {
+		super.name = name;
+		this.aisle = aisle;
+		this.bin = bin;
+		this.pos = pos;
+	}
+}
+
+class Eggs extends Food {
+	int aisle;
+	int bin;
+	int pos;
+	
+	String name = "Eggs";
+	
+	Eggs(int aisle, int bin, int pos) {
+		super.name = name;
+		this.aisle = aisle;
+		this.bin = bin;
+		this.pos = pos;
+	}
 }
